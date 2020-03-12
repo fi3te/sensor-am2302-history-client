1a1b5cdb17ab77d39094d38573ec98db97083ef5
fi3te <fi3te@users.noreply.github.com>
2020-03-12 22:31:11 +0100

    Refactoring

--- date_service.py
+++ date_service.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+import datetime
 from typing import NamedTuple
 
 
@@ -8,6 +8,23 @@ class IsoCalendar(NamedTuple):
     weekday: int
 
 
+def _lstrip_zero(number_str: str) -> int:
+    number_str = number_str.lstrip('0')
+    return int(number_str) if len(number_str) > 0 else 0
+
+
 def file_name_to_iso_calendar(file_name: str) -> IsoCalendar:
-    file_date = datetime.strptime(file_name, '%Y-%m-%d').date()
+    file_date = file_name_to_date(file_name)
     return IsoCalendar(*file_date.isocalendar())
+
+
+def file_name_to_date(file_name: str) -> datetime.date:
+    return datetime.datetime.strptime(file_name, '%Y-%m-%d').date()
+
+
+def parse_time(time: str) -> datetime.time:
+    split_time = time.split(':')
+    hour = _lstrip_zero(split_time[0])
+    minute = _lstrip_zero(split_time[1])
+    second = _lstrip_zero(split_time[2])
+    return datetime.time(hour, minute, second)
