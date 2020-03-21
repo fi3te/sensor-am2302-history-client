053e228eb6ffae242e8a3dfac7841915a1fbc90d
fi3te <w.fiete@web.de>
2020-03-21 22:37:52 +0100

    Option processing improved

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import types
-from typing import NamedTuple, Optional, List
+from abc import ABC
+from typing import List
 
 import backup_service
 import plot_service
@@ -7,46 +8,55 @@ import print_service
 import statistics_service
 
 
-class Option(NamedTuple):
-    key: str
-    description: str
-    executable: bool
-    action: Optional[types.FunctionType]
-    suboptions: Optional[List['Option']] = None
+class Option(ABC):
+    def __init__(self, key: str, description: str):
+        self.key = key
+        self.description = description
+
+
+class ChoiceOption(Option):
+    def __init__(self, key: str, description: str, suboptions: List[Option]):
+        super().__init__(key, description)
+        self.suboptions = suboptions
+
+
+class ActionOption(Option):
+    def __init__(self, key: str, description: str, action: types.FunctionType):
+        super().__init__(key, description)
+        self.action = action
 
 
 all_options = [
-    Option('1', 'Backup files', True, backup_service.start_backup),
-    Option('2', 'Show backup statistics', True, statistics_service.show_statistics),
-    Option('3', 'Create plot', False, None, [
-        Option('1', 'Daily mean plot', True, plot_service.show_daily_mean_plot),
-        Option('2', 'Weekly mean plot', True, plot_service.show_weekly_mean_plot),
-        Option('3', 'Monthly mean plot', True, plot_service.show_monthly_mean_plot)
+    ActionOption('1', 'Backup files', backup_service.start_backup),
+    ActionOption('2', 'Show backup statistics', statistics_service.show_statistics),
+    ChoiceOption('3', 'Create plot', [
+        ActionOption('1', 'Daily mean plot', plot_service.show_daily_mean_plot),
+        ActionOption('2', 'Weekly mean plot', plot_service.show_weekly_mean_plot),
+        ActionOption('3', 'Monthly mean plot', plot_service.show_monthly_mean_plot)
     ])
 ]
 
 
-def process_options(options: List[Option], show_options: bool) -> None:
-    if show_options:
-        print()
-        for option in options:
-            print('%s) %s' % (option.key, option.description))
+def process_options(options: List[Option]) -> None:
+    print()
+    for option in options:
+        print(f'{option.key}) {option.description}')
 
     user_input = input('> ')
-    dictionary = {option.key: (option.action if option.executable else option.suboptions) for option in options}
-    function_or_suboptions = dictionary.get(user_input)
-    if isinstance(function_or_suboptions, types.FunctionType):
-        function_or_suboptions()
-    elif isinstance(function_or_suboptions, List):
-        process_options(function_or_suboptions, True)
+    dictionary = {option.key: option for option in options}
+    chosen_option = dictionary.get(user_input)
+    if isinstance(chosen_option, ChoiceOption):
+        process_options(chosen_option.suboptions)
+    elif isinstance(chosen_option, ActionOption):
+        chosen_option.action()
     else:
-        process_options(options, False)
+        process_options(options)
 
 
 def main() -> None:
     print_service.print_heading('sensor-am2302-history-client')
     print_service.print_subheading('Please choose an option...')
-    process_options(all_options, True)
+    process_options(all_options)
 
 
 if __name__ == '__main__':
