c7ffed00a8e303ef519b3ec9e8d6c81c7038f27a
fi3te <fi3te@users.noreply.github.com>
2020-03-22 13:23:44 +0100

    Option arguments added

--- date_service.py
+++ date_service.py
@@ -1,5 +1,5 @@
 import datetime
-from typing import NamedTuple
+from typing import NamedTuple, Optional
 
 
 class IsoCalendar(NamedTuple):
@@ -28,3 +28,9 @@ def parse_time(time: str) -> datetime.time:
     minute = _lstrip_zero(split_time[1])
     second = _lstrip_zero(split_time[2])
     return datetime.time(hour, minute, second)
+
+
+def file_name_in_interval(file_name: str, from_date: Optional[datetime.date] = None,
+                          to_date: Optional[datetime.date] = None) -> bool:
+    date = file_name_to_date(file_name)
+    return (not from_date or from_date <= date) and (not to_date or date <= to_date)
