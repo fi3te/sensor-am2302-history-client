d33de6dcb048292bef0a8d461edee1f291cc9250
fi3te <fi3te@users.noreply.github.com>
2020-03-27 21:49:59 +0100

    Refactoring

---
+++ aggregation_service.py
@@ -0,0 +1,44 @@
+import statistics
+from typing import List
+
+import filter_service
+from model import MeasurementCollection, MeasurementAggregation, AggregationType
+
+
+def _temperature_list(measurement_collection: MeasurementCollection) -> List[float]:
+    return filter_service.impute_temperature_values_with_mean_of_surroundings(
+        [measurement.temperature for measurement in measurement_collection.measurements])
+
+
+def _humidity_list(measurement_collection: MeasurementCollection) -> List[float]:
+    return [measurement.humidity for measurement in measurement_collection.measurements]
+
+
+def max_value(measurement_collection: MeasurementCollection) -> MeasurementAggregation:
+    return MeasurementAggregation(measurement_collection.tag, AggregationType.MAX,
+                                  max(_temperature_list(measurement_collection)),
+                                  max(_humidity_list(measurement_collection)))
+
+
+def max_values(measurement_collections: List[MeasurementCollection]) -> List[MeasurementAggregation]:
+    return [max_value(measurement_collection) for measurement_collection in measurement_collections]
+
+
+def mean_value(measurement_collection: MeasurementCollection) -> MeasurementAggregation:
+    return MeasurementAggregation(measurement_collection.tag, AggregationType.MEAN,
+                                  statistics.mean(_temperature_list(measurement_collection)),
+                                  statistics.mean(_humidity_list(measurement_collection)))
+
+
+def mean_values(measurement_collections: List[MeasurementCollection]) -> List[MeasurementAggregation]:
+    return [mean_value(measurement_collection) for measurement_collection in measurement_collections]
+
+
+def min_value(measurement_collection: MeasurementCollection) -> MeasurementAggregation:
+    return MeasurementAggregation(measurement_collection.tag, AggregationType.MIN,
+                                  min(_temperature_list(measurement_collection)),
+                                  min(_humidity_list(measurement_collection)))
+
+
+def min_values(measurement_collections: List[MeasurementCollection]) -> List[MeasurementAggregation]:
+    return [min_value(measurement_collection) for measurement_collection in measurement_collections]
--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from datetime import datetime
 from typing import List, Optional, Pattern
 
 import backup_service
-import plot_service
+import matplotlib_service
 import print_service
 import statistics_service
 
@@ -55,11 +55,11 @@ all_options = [
     ActionOption('1', 'Backup files', backup_service.start_backup),
     ActionOption('2', 'Show backup statistics', statistics_service.show_statistics, INTERVAL_ARGUMENT_DEFINITION),
     ChoiceOption('3', 'Create plot', [
-        ActionOption('1', 'Raw plot', plot_service.show_raw_plot, INTERVAL_ARGUMENT_DEFINITION),
-        ActionOption('2', 'Hourly mean plot', plot_service.show_hourly_mean_plot, INTERVAL_ARGUMENT_DEFINITION),
-        ActionOption('3', 'Daily mean plot', plot_service.show_daily_mean_plot, INTERVAL_ARGUMENT_DEFINITION),
-        ActionOption('4', 'Weekly mean plot', plot_service.show_weekly_mean_plot, INTERVAL_ARGUMENT_DEFINITION),
-        ActionOption('5', 'Monthly mean plot', plot_service.show_monthly_mean_plot, INTERVAL_ARGUMENT_DEFINITION)
+        ActionOption('1', 'Raw plot', matplotlib_service.show_raw_plot, INTERVAL_ARGUMENT_DEFINITION),
+        ActionOption('2', 'Hourly mean plot', matplotlib_service.show_hourly_mean_plot, INTERVAL_ARGUMENT_DEFINITION),
+        ActionOption('3', 'Daily mean plot', matplotlib_service.show_daily_mean_plot, INTERVAL_ARGUMENT_DEFINITION),
+        ActionOption('4', 'Weekly mean plot', matplotlib_service.show_weekly_mean_plot, INTERVAL_ARGUMENT_DEFINITION),
+        ActionOption('5', 'Monthly mean plot', matplotlib_service.show_monthly_mean_plot, INTERVAL_ARGUMENT_DEFINITION)
     ])
 ]
 
